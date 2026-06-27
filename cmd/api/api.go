1140c378ea96c54d6e61cb0bb99be0e4fe370c32
HS <harsh.sharma1@kdksoftware.com>
2026-06-27 11:43:57 +0530

    made changes added the chi framework and replaced the gorilla mux

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type application struct {
@@ -14,21 +17,24 @@ type config struct {
 	addr string
 }
 
-func (app *application) mount() *http.ServeMux {
+func (app *application) mount() http.Handler {
 
-	mux := http.NewServeMux()
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 
-	mux.HandleFunc("GET v1/health", app.HealthCheckHandler)
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.HealthCheckHandler)
+	})
 
-	return mux
+	return r
 
 }
 
-func (app *application) run(mux *http.ServeMux) error {
+func (app *application) run(handler http.Handler) error {
 
 	srv := &http.Server{
 		Addr:         app.config.addr,
-		Handler:      mux,
+		Handler:      handler,
 		ReadTimeout:  time.Second * 10,
 		WriteTimeout: time.Second * 20,
 		IdleTimeout:  time.Minute,
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/harshsharma1211/Social
 
 go 1.25.3
+
+require github.com/go-chi/chi/v5 v5.3.0
